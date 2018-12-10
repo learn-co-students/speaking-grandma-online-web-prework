cf4cc4e3c4ae42605cd4d7728f0c5e8be1304855
s greenbaum <gbs4ever2@gmail.com>
2018-12-10 20:24:47 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,16 @@ def speak_to_grandma(phrase)
 
 if phrase  == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-elsif   phrase ==  "Hi Nana, how are you?"
-  return  "HUH?! SPEAK UP, SONNY!"
-elsif  phrase == "Hi!"
+
+elsif   phrase == "Hi Nana, how are you?"
   return"HUH?! SPEAK UP, SONNY!"
-elsif   phrase ==  "phrase".capitalize
-return "HUH?! SPEAK UP, SONNY!"
+
+elsif  phrase == phrase.capitalize
+  return"HUH?! SPEAK UP, SONNY!"
+
+
+elsif   phrase ==  phrase.upcase
+return "NO, NOT SINCE 1938!"
 else
    return  "NO, NOT SINCE 1938!"
 end
