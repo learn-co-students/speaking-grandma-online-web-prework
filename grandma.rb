7521c6738ba3a9c8bd3a289d64124757c439f98f
Marci Clark <marcig612@gmail.com>
2019-01-10 13:29:17 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,18 @@
+describe "#speak_to_grandma" do
+  it "resonds with HUH?!  SPEAK UP, SONNY!  Unless you are shouting"
+  do
+    expect(speak_to_grandma("Hi, grandma!")).to eq "HUH?!  SPEAK UP, SONNY!"
+    elsif
+    it "responds with NO, NOT SINCE 1938! When she can hear you" do
+      expect(speak_to_grandma("How are you?")).to eq "NO, NOT SINCE 1938!"
+      elsif
+      it "responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!" do
+        expect(speak_to_grandma("I LOVE YOU GRANDMA!")).to eq "I LOVE YOU TOO PUMPKIN!"
+
+
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
