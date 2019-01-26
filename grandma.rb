7e3a4508b1d2e1a04c01b3fa9b7f7ddbbfbed7c9
Chance Smith <chancesmith97@hotmail.com>
2019-01-26 01:29:42 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(greeting)
+  if greeting =="I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif greeting == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif greeting == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif greeting == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  elsif greeting == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  end
+end
