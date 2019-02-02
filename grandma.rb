dd43f64f6277881967ee8d9a206d40b51f8ab015
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-02 07:13:04 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 def speak_to_grandma(phrase)
-  if(phrase == phrase.upcase)
+  if(phrase != phrase.upcase)
     return 'HUH?! SPEAK UP, SONNY!'
   elsif(phrase == 'I LOVE YOU GRANDMA!')
     return 'I LOVE YOU TOO PUMPKIN!'
   else
     return 'NO, NOT SINCE 1938!'
+  end
 end
 
-speak_to_grandma()
 
 # Write a speak_to_grandma method.
 
