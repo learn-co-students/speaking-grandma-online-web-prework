3089d6af26e19ccfa0aa781e1641d8c62073e861
Kat B <katreesebarnes@gmail.com>
2019-02-03 14:23:18 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,11 @@
+
+ def speak_to_grandma
+   
+if  speak_to_grandma == "I LOVE YOU GRANDMA!"
+  
+  puts "HUH?! SPEAK UP, SONNY!"
+  
+end
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
