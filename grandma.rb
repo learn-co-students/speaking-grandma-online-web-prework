77dd1e6153c82f09ea0f52045bd544cf994499d3
mark baruch <mbaruch28@gmail.com>
2019-02-04 17:23:53 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,8 @@
-# Write a speak_to_grandma method.
+how is life grandma.uppercase == "I LOVE YOU GRANDMA"
+if how is life grandma.uppercase == "I LOVE YOU GRANDMA"
+  puts "I LOVE YOU, TOO PUMPKIN"
+end
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+else "how is life grandma.uppercase" == "HUH?! SPEAK UP, SONNY"
+ puts "I LOVE YOU, TOO PUMPKIN
+end
