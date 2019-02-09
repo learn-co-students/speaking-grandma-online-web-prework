0d4eb95126df6ea0c4fa2ce066bb0480c23f0409
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-09 20:18:48 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,10 @@
-# Write a speak_to_grandma method.
+phrase = "I LOVE YOU GRANDMA!".upcase
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma
+  if phrase == "i love you grandma".downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elseif phrase == "I love you Grandma!".capitalize
+  return "NO, NOT SINCE 1938!"
+else phrase == "I love you grandma!".upcase
+  return "I LOVE YOU TOO PUMPKIN!"
+end
