bc03359656b3f6a3357dbafd17174f306d5009f2
AJ Guiles <ashiguiles@gmail.com>
2019-02-19 03:07:48 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,11 @@
 def speak_to_grandma(phrase)
       if phrase != phrase.upcase
-        
         return "HUH?! SPEAK UP, SONNY!"
-          
-      
-       elsif phrase =="I LOVE YOU GRANDMA!"
+       elsif phrase == "I LOVE YOU GRANDMA!"
           return "I LOVE YOU TOO PUMPKIN!"
        else phrase == phrase.upcase
           return  "NO, NOT SINCE 1938!"
-       
        end
       end 
 
-       speak_to_grandma("I LOVE YOU GRANDMA!")
+     
