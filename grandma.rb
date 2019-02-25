4a4a5f683c5c06e03843c40417f00e65629ebec8
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-25 14:39:36 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,10 @@ def speak_to_grandma (phrase)
     return 'I LOVE YOU TOO PUMPKIN!'
   end
   if phrase != phrase.upcase
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   else 
     return "NO, NOT SINCE 1938!"
+  end
 end
   # Write a speak_to_grandma method.
 
