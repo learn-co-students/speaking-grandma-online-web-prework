4ae50d141bd715f8d5432400b5d3d2d56e094858
Stephanie Purple <spurple133@gmail.com>
2019-03-03 18:01:10 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,9 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-
-elsif phrase == phrase.downcase
-  return "HUH?! SPEAK UP SONNY!"
-
-else phrase == phrase.downcase
-  return "HUH?! SPEAK UP SONNY!"
-
-elsif phrase == phrase.upcase
-  return  "NO, NOT SINCE 1938!"
-
+elsif phrase != phrase.upcase
+  return "HUH?! SPEAK UP, SONNY!"
 else
-  return "NO, NOT SINCE 1938!"
- end
+  return"NO, NOT SINCE 1938!"
+end
 end
