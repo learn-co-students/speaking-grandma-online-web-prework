b65968d83f60481a5aea2979e8259f5d2bc99b25
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-19 19:12:56 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,23 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(cutiepie) #method definition
+if cutiepie == 'I LOVE YOU GRANDMA!'
+aww = 'I LOVE YOU TOO PUMPKIN!'
+puts aww
+aww
+
+elsif cutiepie == cutiepie.upcase
+  oldie = 'NO, NOT SINCE 1938!'
+  puts oldie
+  oldie
+
+else 
+  shout = 'HUH?! SPEAK UP, SONNY!'
+  puts shout
+  shout
+end
+end 
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
