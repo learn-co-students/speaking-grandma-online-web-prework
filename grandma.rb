06d01796963b5aae83f52e5518a5f5f2de274123
James Pier <jpier2012@gmail.com>
2019-04-01 00:50:34 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,24 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma statement
+  if statement != statement.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif statement == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+end
+
+puts speak_to_grandma "ALL CAPS"
+puts speak_to_grandma "lowercase"
+puts speak_to_grandma "I LOVE YOU GRANDMA!"
