00a84feb8bf40f2c7007dc52e94d42aae6cfe141
Angela Lo <angelawenlo@gmail.com>
2019-04-18 17:44:49 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,20 @@
-# Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(response)
+  response= "I LOVE YOU GRANDMA!"
+  if response=="I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+end
+  response="Hi Nana, how are you?"
+  if response== "Hi, Nana"
+    puts= "Hi, Pumpkin"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+end
+  response= "Hi!"
+  if response=="HI"
+    puts= "HI,PUMPKIN"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+end
+  response=
