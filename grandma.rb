6bcd141e8f8ae13bd64b022886280d350a2bb564
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-04 00:30:58 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif speak_to_grandma == .downcase
+  puts "HUH?! SPEAK UP, SONNY!"
+else speak_to_grandma == .upcase
+  puts "NO, NOT SINCE 1938!"
+end
