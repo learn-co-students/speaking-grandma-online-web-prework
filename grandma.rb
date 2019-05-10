274e41dc1065b90197f9e9571611ba45dd5be08a
Justin <sumbhodi1618@gmail.com>
2019-05-10 23:22:25 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
-def speak_to_grandma
-  speak = gets
-  if speak == "Hi Nana, how are you?"
+def speak_to_grandma(speak)
+  if speak == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif speak != speak.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif speak == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
-  else speak == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!".upcase
+  else
+    return "NO, NOT SINCE 1938!" 
   end
- end
+end
