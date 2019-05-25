7e4dd9d263f2a5c9e2592fcefe73878d4d86470a
Adam Ervin <adamjervin@gmail.com>
2019-05-25 22:23:21 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,31 +1,15 @@
-def
-speak_to_grandma = "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase) 
+  if (phrase == "I LOVE YOU GRANDMA!")
+     "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase == phrase.upcase)
+     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+  
 end
+  
+  
 
-if speak_to_grandma == "hey ma"
-puts "HUH?! SPEAK UP SONNY!"
+  
 
-elsif speak_to_grandma == "HEY"
-puts "NO NOT SINCE 1938!"
-
-elsif speak_to_grandma == "I LOVE YOU GRANDMA"
-puts " I LOVE YOU TOO PUMPKIN"
-
-else 
-puts "HUH?! SPEAK UP SONNY"
-
-end
-
-
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
